a0c1634fa1d8c747927bd64d9548e11e085e68b5
이창민 <changmin@ichangmin-ui-MacBookPro.local>
2025-01-08 17:26:38 +0900

    feat: swagger setting

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
     .setTitle('User Posts API')
     .setDescription('API documentation for user posts management')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
